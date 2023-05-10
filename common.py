ccc2574c38a09fbca5e51c0cecfa9d82d52f9804
USSRN-CR <1210841149@qq.com>
2023-05-11 00:02:01 +0800

    提交文件

---
+++ common.py
@@ -0,0 +1,229 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import nn, cuda
+from torch.autograd import Variable
+from torch.nn import Module, Sequential, Conv2d, ReLU,AdaptiveMaxPool2d, AdaptiveAvgPool2d, \
+    NLLLoss, BCELoss, CrossEntropyLoss, AvgPool2d, MaxPool2d, Parameter, Linear, Sigmoid, Softmax, Dropout, Embedding
+
+
+class MBblock(nn.Module):
+    def __init__(
+        self, n_feats):
+        super(MBblock, self).__init__()
+        self.c1 = default_conv(in_channels=n_feats,out_channels=n_feats,kernel_size=3)
+        self.act1 = nn.PReLU()
+        self.c_depth = nn.Conv2d(in_channels=n_feats,out_channels=n_feats,kernel_size=3,
+                                 stride=1,
+                                 padding=1,
+                                 groups=n_feats
+                                 )
+        self.act2 = nn.PReLU()
+        self.c_point = default_conv(in_channels=n_feats,out_channels=n_feats,kernel_size=1)
+
+    def forward(self, x):
+        out = self.c1(x)
+        out = self.act1(out)
+        out = self.c_depth(out)
+        out = self.act2(out)
+        out = self.c_point(out)
+        return out + x
+    
+def default_conv(in_channels, out_channels, kernel_size,stride=1, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2),stride=stride, bias=bias)
+
+def default_conv_my_conv(in_channels, out_channels, kernel_size,stride=1, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2),stride=stride, bias=bias)
+
+class BasicBlock(nn.Sequential):
+    def __init__(
+        self, conv, in_channels, out_channels, kernel_size, stride=1, bias=True,
+        bn=False, act=nn.PReLU()):
+
+        m = [conv(in_channels, out_channels, kernel_size, bias=bias)]
+        if bn:
+            m.append(nn.BatchNorm2d(out_channels))
+        if act is not None:
+            m.append(act)
+
+        super(BasicBlock, self).__init__(*m)
+    
+class ResBlock(nn.Module):
+    def __init__(
+        self, conv, n_feats, kernel_size,
+        bias=True, bn=False, act=nn.ReLU(), res_scale=0.1):
+
+        super(ResBlock, self).__init__()
+        m = []
+        for i in range(3):
+            m.append(nn.Conv2d(n_feats, n_feats, kernel_size,padding=1, bias=bias))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feats))
+            if i != 2:
+                m.append(act)
+
+        self.body = nn.Sequential(*m)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x).mul(self.res_scale)
+        res += x
+        return res
+
+class Upsampler(nn.Sequential):
+    def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
+
+        m = []
+        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
+            for _ in range(int(math.log(scale, 2))):
+                m.append(conv(n_feats, 4 * n_feats, 3, bias))
+                m.append(nn.PixelShuffle(2))
+                if bn:
+                    m.append(nn.BatchNorm2d(n_feats))
+                if act == 'relu':
+                    m.append(nn.ReLU(True))
+                elif act == 'prelu':
+                    m.append(nn.PReLU(n_feats))
+
+        elif scale == 3:
+            m.append(conv(n_feats, 9 * n_feats, 3, bias))
+            m.append(nn.PixelShuffle(3))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feats))
+            if act == 'relu':
+                m.append(nn.ReLU(True))
+            elif act == 'prelu':
+                m.append(nn.PReLU(n_feats))
+        else:
+            raise NotImplementedError
+
+        super(Upsampler, self).__init__(*m)
+
+class GateConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, transpose=False):
+        super(GateConv, self).__init__()
+        self.out_channels = out_channels
+        if transpose:
+            self.gate_conv = nn.ConvTranspose2d(in_channels, out_channels * 2,
+                                                kernel_size=kernel_size,
+                                                stride=stride, padding=padding)
+        else:
+            self.gate_conv = nn.Conv2d(in_channels, out_channels * 2,
+                                       kernel_size=kernel_size,
+                                       stride=stride, padding=padding)
+	  self.tail=nn.ReLU()
+
+    def forward(self, x):
+        x = self.gate_conv(x)
+        (x, g) = torch.split(x, self.out_channels, dim=1)
+        return self.tail(x) * torch.sigmoid(g)
+
+class CAM_Module(Module):
+    """ Channel attention module"""
+
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.gamma = Parameter(torch.zeros(1))  
+        self.softmax = Softmax(dim=-1)
+
+    def forward(self, x):
+
+        m_batchsize, C, height, width = x.size()
+        # A -> (N,C,HW)
+        proj_query = x.view(m_batchsize, C, -1)
+        # A -> (N,HW,C)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+
+        energy = torch.bmm(proj_query, proj_key)
+
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy) - energy
+
+        attention = self.softmax(energy_new)
+        # A -> (N,C,HW)
+        proj_value = x.view(m_batchsize, C, -1)
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma * out + x
+        return out
+
+
+class EfficientAttention(nn.Module):
+    def __init__(self, in_channels, dim, head_count, out_channels):
+        super().__init__()
+        self.in_channels = in_channels
+        self.head_count = head_count
+        self.dim = dim
+        self.gamma = Parameter(torch.zeros(1))
+
+        self.keys = nn.Conv2d(in_channels, dim, 1)
+        self.queries = nn.Conv2d(in_channels, dim, 1)
+        self.values = nn.Conv2d(in_channels, dim, 1)
+        if dim != out_channels:
+            self.reprojection = nn.Conv2d(dim, out_channels, 1)
+        else:
+            self.reprojection = None
+
+    def forward(self, input_, mask=None, return_scores=False):
+        n, _, h, w = input_.size()
+        keys = self.keys(input_)
+        queries = self.queries(input_)
+        values = self.values(input_)
+        head_channels = self.dim // self.head_count
+
+        if mask is not None:
+            # [b,1,h,w]
+            mask = F.interpolate(mask, size=[h, w], mode='nearest')
+            keys += (mask * -10000.0)
+            queries += (mask * -10000.0)
+
+        keys = keys.reshape((n, self.dim, h * w))  # [b,d,h*w]
+        queries = queries.reshape(n, self.dim, h * w)
+        values = values.reshape((n, self.dim, h * w))
+
+        attended_values = []
+        scores = 0
+        for i in range(self.head_count):
+            key = F.softmax(keys[:, i * head_channels: (i + 1) * head_channels, :], dim=2)
+            query = F.softmax(queries[:, i * head_channels: (i + 1) * head_channels, :], dim=1)
+            value = values[:, i * head_channels: (i + 1) * head_channels, :]
+            context = key @ value.transpose(1, 2)  # [b, d, d]
+            attended_value = (context.transpose(1, 2) @ query).reshape(n, head_channels, h, w)
+            attended_values.append(attended_value)
+            if return_scores:
+                score = torch.matmul(query.transpose(1, 2), key)  # [b, hw, hw]
+                score = torch.mean(score, dim=1).reshape([n, h, w])
+                scores += score
+
+        aggregated_values = torch.cat(attended_values, dim=1)
+        if self.reprojection is not None:
+            reprojected_value = self.reprojection(aggregated_values)
+        else:
+            reprojected_value = aggregated_values
+
+        attention = self.gamma * reprojected_value + input_
+
+        if return_scores:
+            max_value, _ = torch.max(scores.reshape([n, h * w]), dim=1)
+            max_value = max_value[:, None, None]
+            scores = scores / (max_value + 1e-5)
+            scores = scores.unsqueeze(1)
+            scores = scores.detach()
+            return attention, scores
+        else:
+            return attention
+
+
+if __name__ == '__main__':
+    net = MBblock(in_channels=4).cuda()
+    data = torch.FloatTensor(size=(2,4,16,16)).cuda()
+    print(net(data).shape)
+
---
+++ dagl.py
@@ -0,0 +1,195 @@
+from numpy.lib.function_base import delete
+import model.common as common
+import torch.nn as nn
+import torch
+import math
+import torch.nn.functional as F
+import time
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import spectral.io.envi as envi
+import cv2
+from skimage.feature import canny
+import math
+import os
+import pandas as pd
+from tqdm import tqdm
+
+
+def make_model(args, parent=False):
+    return RR(args)
+
+
+class RR(nn.Module):
+    def __init__(self, args, conv=common.default_conv):
+        super(RR, self).__init__()
+        # define basic setting
+        n_resblocks = args.n_resblocks
+        n_feats = args.n_feats
+        kernel_size = 3
+
+        my_head1_ms = [common.GateConv(3, 72, kernel_size, padding=1)]
+        my_head1_sar = [conv(3, 72, 3)]  
+        my_head2_M3_ms=[common.GateConv(72, 24, 3, stride=1, padding=1)]
+        my_head2_M5_ms=[common.GateConv(72, 24, 5, stride=1, padding=2)]
+        my_head2_M7_ms=[common.GateConv(72, 24, 7, stride=1, padding=3)]
+        my_head3_M3_sar=[nn.Conv2d(72, 24, 3, stride=1, padding=1)]
+        my_head3_M5_sar=[nn.Conv2d(72, 24, 5, stride=1, padding=2)]
+        my_head3_M7_sar=[nn.Conv2d(72, 24, 7, stride=1, padding=3)]
+        my_head1_canny = [conv(2, 36, 3)]
+        my_head4 = [conv(180, n_feats, 3)]
+        my_head5 = [nn.ReLU(inplace=True)]
+
+        n_resblocks = 8
+        m_body = [
+            common.ResBlock(
+                conv, n_feats, kernel_size, nn.ReLU(), res_scale=args.res_scale 
+            ) for _ in range(n_resblocks // 2)
+        ]
+        for i in range(1):
+            m_body.append(
+                SACA(in_channels=n_feats,res_scale=args.res_scale))  
+
+        for i in range(n_resblocks // 2):
+            m_body.append(
+                common.ResBlock(conv, n_feats, kernel_size, nn.ReLU(), res_scale=args.res_scale))  
+        m_tail = [
+            conv(n_feats, args.n_colors, kernel_size)
+        ]
+
+        self.my_head1_ms = nn.Sequential(*my_head1_ms)
+        self.my_head1_sar = nn.Sequential(*my_head1_sar)
+        self.my_head2_M3_ms = nn.Sequential(*my_head2_M3_ms)
+        self.my_head2_M5_ms = nn.Sequential(*my_head2_M5_ms)
+        self.my_head2_M7_ms = nn.Sequential(*my_head2_M7_ms)
+        self.my_head3_M3_sar = nn.Sequential(*my_head3_M3_sar)
+        self.my_head3_M5_sar = nn.Sequential(*my_head3_M5_sar)
+        self.my_head3_M7_sar = nn.Sequential(*my_head3_M7_sar)
+        self.head4 = nn.Sequential(*my_head4)
+        self.head5 = nn.Sequential(*my_head5)
+        self.my_head1_canny = nn.Sequential(*my_head1_canny)
+        self.body = nn.Sequential(*m_body)
+        self.tail = nn.Sequential(*m_tail)
+
+    def forward(self, x):
+        trainer_width = x.shape[2]
+        trainer_band = x.shape[1]
+        batch = x.shape[0]
+        x1 = x[:, 0:trainer_band-3]  
+        x2 = x[:, trainer_band-3:trainer_band]  
+        x_ms = x1.reshape(batch, trainer_band-3, trainer_width, trainer_width)
+
+        Canny_1 = extract_canny(x2[:,0,:,:],batch)
+        Canny_2 = extract_canny(x2[:,1,:,:],batch)
+        Canny_1=torch.from_numpy(Canny_1)
+        Canny_2=torch.from_numpy(Canny_2)
+        canny = torch.cat((Canny_1,Canny_2), 1)
+        canny=canny.cuda()
+        x_sar = x2.reshape(batch, 3, trainer_width, trainer_width)
+        res1_ms = self.my_head1_ms(x_ms)
+        res1_sar = self.my_head1_sar(x_sar)
+        res2_ms3 = self.my_head2_M3_ms(res1_ms)
+        res2_ms5 = self.my_head2_M5_ms(res1_ms)
+        res2_ms7 = self.my_head2_M7_ms(res1_ms)
+        res2_sar3 = self.my_head3_M3_sar(res1_sar)
+        res2_sar5 = self.my_head3_M5_sar(res1_sar)
+        res2_sar7 = self.my_head3_M7_sar(res1_sar)
+        res2_contact_ms = torch.cat((res2_ms3, res2_ms5, res2_ms7), 1)
+        res2_contact_sar = torch.cat((res2_sar3, res2_sar5, res2_sar7), 1)
+        res2_all_ms = res1_ms+res2_contact_ms
+        res2_all_sar = res1_sar+res2_contact_sar
+        canny=self.my_head1_canny(canny)
+        res=torch.cat((res2_all_ms, res2_all_sar,canny), 1)
+        res=self.head4(res)
+        res = self.head5(res)
+        res = self.body(res)
+        res = self.tail(res)
+        return res + x1
+
+
+    def load_state_dict(self, state_dict, strict=True):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                if isinstance(param, nn.Parameter):
+                    param = param.data
+                try:
+                    own_state[name].copy_(param)
+                except Exception:
+                    if name.find('tail') == -1:
+                        raise RuntimeError('While copying the parameter named {}, '
+                                           'whose dimensions in the model are {} and '
+                                           'whose dimensions in the checkpoint are {}.'
+                                           .format(name, own_state[name].size(), param.size()))
+            elif strict:
+                if name.find('tail') == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'
+                                   .format(name))
+
+class SACA(nn.Module):
+    def __init__(self, in_channels,res_scale, num=4,conv=common.default_conv):
+        super(SACA, self).__init__()
+        n_feats = in_channels
+        kernel_size = 3
+        self.RBS1 = common.ResBlock(conv, n_feats, kernel_size, nn.ReLU(), res_scale=res_scale)
+        self.attention = common.EfficientAttention(n_feats, n_feats, head_count=num, out_channels=n_feats)
+        self.attention2 = common.CAM_Module(n_feats)
+        self.RBS2 = common.ResBlock(conv, n_feats, kernel_size, nn.ReLU(), res_scale=res_scale)
+    def forward(self, x):
+
+        rb1=self.RBS1(x)
+        a1 = self.attention(rb1)
+        a2 = self.attention2(rb1)
+        rb2 = self.RBS2(a1+a2)
+        return rb2
+
+"""
+Fundamental functions
+"""
+def extract_canny(images,batch):
+    images=images.cpu()
+    img_edge2out = np.zeros_like(images)
+    img_edge2out = img_edge2out[:, np.newaxis, :, :]
+    for i in range(batch):
+        LC = images[i,:,:].cpu().detach().numpy()
+        out = np.zeros_like(LC)
+        LC = LC.astype(np.float)
+        out=cv2.normalize(LC, None, 0, 255, cv2.NORM_MINMAX)
+        out = out.astype(np.uint8)
+        img_edge2 = canny(out, sigma=2).astype(np.float)
+        img_edge2 = img_edge2.astype(np.float32)
+        img_edge2out[i,0,:,:] = cv2.normalize(img_edge2, None, 0, 1, cv2.NORM_MINMAX)
+    return img_edge2out
+
+
+
+def matrix(image, conv_ker):
+    res = (image * conv_ker).sum()
+    res = res / (conv_ker.shape[0] * conv_ker.shape[1])
+    if (res < 0):
+        res = 0
+    elif res > 255:
+        res = 255
+    return res
+
+
+def conv_operation(image):
+    conv_kernel = np.array([[1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1]])  # 7*7膨胀。
+    conv_kernel_heigh = conv_kernel.shape[0]
+    conv_kernel_width = conv_kernel.shape[1]
+    conv_heigh = image.shape[0] - conv_kernel.shape[0] + 1  # 确定卷积结果的大小
+    conv_width = image.shape[1] - conv_kernel.shape[1] + 1
+    conv_ret = np.zeros((conv_heigh, conv_width), np.uint8)
+
+    for i in range(conv_heigh):
+        for j in range(conv_width):
+            conv_ret[i, j] = matrix(image[i:(i + conv_kernel_heigh), j:(j + conv_kernel_width)], conv_kernel)
+
+    return conv_ret
